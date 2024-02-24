9a32d270972a09e7ade0cd8ed8f11517b5d515ac
Ashish Kumar <akdev5520@gmail.com>
2024-02-24 15:31:04 +0530

    added navbar and header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+	return (
+		<div className="flex bg-orange-500">
+			<div className="w-full md:w-1/2 p-12 md:p-16 flex flex-col justify-center items-center">
+				<h1 className="font-bold text-4xl md:text-9xl text-white">
+					Welcome to Pet Finder
+				</h1>
+			</div>
+			<div className="hidden md:block w-1/2 px-6">
+				<img
+					src="public/dog-cover-header.png"
+					className="w-full h-full object-cover"
+					alt="dog"
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
